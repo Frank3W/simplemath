e7de2cb8d66315912d7338185438e7b6d1da26b4
zhihan <zhihan.gao@yahoo.com>
2021-03-17 23:19:47 -0400

    add sequence functions

--- simplemath/numbers.py
+++ simplemath/numbers.py
@@ -1,3 +1,37 @@
 import numpy as np
 
 EXP = np.e
+
+class NotPositiveInt(ValueError):
+    def __init__(self, message):
+        self.message = message
+        super.__init__(self.message)
+
+
+def get_fibonacci(n):
+
+    if n <= 0 or n != int(n):
+        raise NotPositiveInt('Input n must be a positive integer.')
+
+    if n == 1:
+        return [1]
+    elif n == 2:
+        return [1, 1]
+    else:
+        r_list = [1, 1]
+        for idx in range(2, n):
+            r_list.append(r_list[-1] + r_list[-2])
+
+        return r_list
+
+def get_factorial(n):
+    if n <= 0 or n != int(n):
+        raise NotPositiveInt('Input n must be a positive integer.')
+
+    if n == 1:
+        return [1]
+    else:
+        r_list = [1]
+        for idx in range(1, n):
+            r_list.append(r_list[-1] * (idx+1))
+        return r_list
