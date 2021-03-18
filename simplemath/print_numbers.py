2ad857a89481ce91e600614bca4d10cf578168c7
zhihan <zhihan.gao@yahoo.com>
2021-03-17 23:37:34 -0400

    remove hard coded package name reference

--- simplemath/print_numbers.py
+++ simplemath/print_numbers.py
@@ -7,7 +7,11 @@ from simplemath.numbers import EXP
 
 def main():
     print(f'Natural Exp Number: {EXP}')
-    rs = pkg_resources.resource_stream('simplemath', 'data/seq.csv')
+
+    # " Note that if a module name is used, then the resource name
+    # is relative to the package immediately containing the named module."
+    # Here, __name__ is simplemath.print_numbers
+    rs = pkg_resources.resource_stream(__name__, 'data/seq.csv')
     utf8_reader = codecs.getreader('utf-8')
     seq_pd = pd.read_csv(utf8_reader(rs))
     print(seq_pd)
